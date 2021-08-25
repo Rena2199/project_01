672a0172da7de308e50862196d7b546968f22b7e
Rena <17809276984@163.com>
2021-08-25 11:56:23 +0800

    完成了登录和注册功能开发

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+$(function () {
+    // 点击去注册账号的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    // 点击去登录链接
+    $('#link_login').on('click', function () {
+        $('.reg-box').hide()
+        $('.login-box').show()
+    })
+})
